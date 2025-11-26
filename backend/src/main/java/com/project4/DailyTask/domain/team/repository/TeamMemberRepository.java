90d421f6f06eb141bd057ad0192307ca712e6259
asdf-qwe <fism@naver.com>
2025-11-26 19:50:34 +0900

    be/feat/3 - TEAM - 팀 수정 api

--- backend/src/main/java/com/project4/DailyTask/domain/team/repository/TeamMemberRepository.java
+++ backend/src/main/java/com/project4/DailyTask/domain/team/repository/TeamMemberRepository.java
@@ -4,7 +4,10 @@ import com.project4.DailyTask.domain.team.entity.Role;
 import com.project4.DailyTask.domain.team.entity.TeamMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TeamMemberRepository extends JpaRepository<TeamMember, Long> {
     boolean existsByTeamIdAndUserIdAndRole(Long teamId, Long userId, Role role);
     boolean existsByTeamIdAndUserId(Long teamId, Long userId);
+    Optional<TeamMember> findByTeamIdAndRole(Long teamId, Role role);
 }
